0114e1bd3f11310e82b8da08231cb8350d0532aa
Laurent Fenu <laurentfenu.devweb@gmail.com>
2024-05-20 18:13:45 +0200

    create a car detail page

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -9,6 +9,9 @@ use App\Repository\CarRepository;
 
 class CarController extends AbstractController
 {
+     /**
+     * Page d'accueil
+     */
     #[Route('/', name: 'app_home')]
     public function index(CarRepository $carRepository): Response
     {
@@ -17,5 +20,32 @@ class CarController extends AbstractController
         return $this->render('home.html.twig', [
             'cars' => $cars,
         ]);
+    }
+
+    /**
+     * Page de détail d'une voiture
+     */
+    #[Route('/car/{id}', name: 'app_car')]
+    public function car(CarRepository $carRepository, int $id): Response
+    {
+        $car = $carRepository->find($id);
+
+        if(!$car) {
+            // return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_error');
+        }
+
+        return $this->render('car.html.twig', [
+            'car' => $car,
+        ]);
+    }
+
+    /**
+     * Page d'erreur
+     */
+    #[Route('/errorPage', name: 'app_error')]
+    public function error(): Response
+    {
+        return $this->render('error.html.twig', ['error' => 'Cette voiture est inexixtante']); 
     }
 }
