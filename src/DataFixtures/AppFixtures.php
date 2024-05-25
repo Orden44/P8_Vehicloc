61992ff98d6d3d633f77156800752abcf8c24c26
Laurent Fenu <laurentfenu.devweb@gmail.com>
2024-05-25 12:07:27 +0200

    add fake cars to create montage data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,11 +10,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = (new \Faker\Factory())::create();
+        $faker->addProvider(new \Faker\Provider\FakeCar($faker));
         // create 5 cars!
         for ($i = 1; $i < 6; $i++) {
             $car = new Car();
-            $car->setName('Voiture '.$i);
-            $car->setContent('Description de la voiture '.$i);
+            $car->setName($faker->vehicle());
+            $car->setContent($car->getName().' est une voiture '.$faker->vehicleFuelType().' de type '.$faker->vehicleType());
             $car->setDailyPrice(round(30 + mt_rand() / mt_getrandmax() * (600 - 580), 2));
             $car->setMonthlyPrice(round(300 + mt_rand() / mt_getrandmax() * (10000 - 9000), 2));
             $car->setPlaces(mt_rand(1, 9));
